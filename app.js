f39e7c6e911e4c569d723afeee096053a1864eef
joselmm <josealfredosoraca@gmail.com>
2024-08-26 16:00:47 -0500

    funciona

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from "express";
 import { readFile, writeFile } from 'node:fs/promises';
 import path from "path";
 import fetch from "node-fetch";
-import {generateCardFromString} from "./GenCard.js";
+import { generateCardFromString } from "./GenCard.js";
 import { resolve } from 'node:path';
 
 
@@ -17,15 +17,17 @@ const app = express()
 const port = 3000;
 var qrSaved = false;
 var logged = false;
-var chat_id="#-1002235041204"
+var chat_id = "#-1002235041204"
 var regexCard = /\d{16,}\|(\d{1}|\d{2})\|(\d{2}|\d{4})\|(\d{3,4})/g;
 
 var maxCurrent = 2;
 
 
 // Launch the browser and open a new blank page
-const browser = await puppeteer.launch({headless:false, defaultViewport: null,
-  args: ['--start-maximized'] });
+const browser = await puppeteer.launch({
+  headless: false, defaultViewport: null,
+  args: ['--start-maximized']
+});
 const defaultBrowserContext = browser.defaultBrowserContext();
 const pages = await defaultBrowserContext.pages();
 const page1 = pages[0]; // Seleccionar la primera pestaña (la pestaña que se abre al abrir el navegador)
@@ -33,46 +35,64 @@ const page1 = pages[0]; // Seleccionar la primera pestaña (la pestaña que se a
 await page1.goto('https://web.telegram.org/a/');
 
 var localStorageFileId = "1hxdL8nBIuMzUU-TvKJBNeIXXb7EZM-fy";
-var ftpServerUrl= "https://script.google.com/macros/s/AKfycbz9GV4R7FOQOoTukIl8RDmdqw_sOy00z8H1IJDgA8dCQIMCbxO031VFF4TbwjSqBf0PIg/exec";
+var ftpServerUrl = "https://script.google.com/macros/s/AKfycbz9GV4R7FOQOoTukIl8RDmdqw_sOy00z8H1IJDgA8dCQIMCbxO031VFF4TbwjSqBf0PIg/exec";
 
-var localStorageJSON =await fetch("https://drive.google.com/uc?id="+localStorageFileId)
-.then(response => response.text())
-.catch(error => {
-  console.error('Error:', error)
-  return "";
-});
-if(localStorageJSON!==""){
+var localStorageJSON = await fetch("https://drive.google.com/uc?id=" + localStorageFileId)
+  .then(response => response.text())
+  .catch(error => {
+    console.error('Error:', error)
+    return "";
+  });
+if (localStorageJSON !== "") {
 
   var localStorageObject = JSON.parse(localStorageJSON);
   await page1.evaluate((localStorageJSON) => {
     Object.keys(localStorageJSON).forEach(key => {
       window.localStorage.setItem(key, localStorageJSON[key]);
     });
-  },localStorageObject);
+  }, localStorageObject);
 }
 var page = await browser.newPage();
 page1.close();
 // Navigate the page to a URL.
-await page.goto('https://web.telegram.org/a/'+chat_id);
+await page.goto('https://web.telegram.org/a/' + chat_id);
 
 
 page.on('dialog', async (dialog) => {
-    await dialog.accept();
+  await dialog.accept();
 });
 
 // Set screen size.
 
-async function antibot() {
-  var soyHumanoIndex=await page.evaluate(()=>{
-    var soyHumanoIndex = Array.from(document.querySelectorAll("#message-1409 > div.message-content-wrapper.can-select-text > div.InlineButtons > div > button")).findIndex(e=>e.innerText==='Soy aHumano ');
-    if(soyHumanoIndex>=0){
-      return soyHumanoIndex+1
+function antibot() {
+  return new Promise(async (resolve) => {
+
+    var classForAntibotButton = "antibot-button";
+    var antibotBtnArrayLength = await page.evaluate((classForAntibotButton) => {
+      var antibotBtnArray = Array.from(document.querySelectorAll("div > div.message-content-wrapper.can-select-text > div.InlineButtons > div > button")).filter(e => e.innerText === 'Soy Humano ');
+      if (antibotBtnArray.length > 0) {
+        for (let i = 0; i < antibotBtnArray.length; i++) {
+          antibotBtnArray[i].classList.add(classForAntibotButton + "-" + i);
+        }
+        return antibotBtnArray.length;
+      } else {
+        return null
+      }
+    }, classForAntibotButton);
+
+    if (antibotBtnArrayLength) {
+      for (let i = 0; i < antibotBtnArrayLength; i++) {
+        await page.click("." + classForAntibotButton + "-" + i);
+        console.log("se dio click a: " + i);
+      }
+      var page1 =await browser.newPage();
+      await page.close();
+      page = page1;
+      await page.goto('https://web.telegram.org/a/' + chat_id);
     }
-    return null
-  });
-  if(soyHumanoIndex!==null && soyHumanoIndex>=1){
-    await page.click();
-  }
+    resolve(classForAntibotButton);
+  
+  })
 }
 
 /* MANEJAR TARJETAS */
@@ -85,14 +105,14 @@ async function manejarTarjetas() {
   }, chat_id)
   */
   var queue = [];
-  async function interval(){
-    var startTime=Date.now();
+  async function interval() {
+    var startTime = Date.now();
 
-    antibot();
-    
-/*     var checkingCards=await getCheckingCards();
-    console.log("checking cards: ",checkingCards) */
-    var cardsStatuses= await getCardsStatuses();
+    await antibot();
+
+    /*     var checkingCards=await getCheckingCards();
+        console.log("checking cards: ",checkingCards) */
+    var cardsStatuses = await getCardsStatuses();
 
     console.log(cardsStatuses)
     console.log(queue)
@@ -108,24 +128,25 @@ async function manejarTarjetas() {
       }
     } */
 
-    for (let i = cardsStatuses.length-1; i >=0; i--) {
+    for (let i = cardsStatuses.length - 1; i >= 0; i--) {
       //console.log(i+1)
-        const cardObject = cardsStatuses[i];
-        if(queue.indexOf(cardObject.card)>=0) {
-            //console.log(cardObject)
-            queue.splice(queue.indexOf(cardObject.card), 1)
+      const cardObject = cardsStatuses[i];
+      if (queue.indexOf(cardObject.card) >= 0) {
+        //console.log(cardObject)
+        queue.splice(queue.indexOf(cardObject.card), 1)
+      }
+      if (cardObject.live) {
+        var fileContent = await readFile(resolve("./lives.json"))
+        var liveCards = JSON.parse(fileContent);
+        if (!liveCards.find(e => e.card === cardObject.card)) {
+          liveCards.push(cardObject);
+          await writeFile(resolve("./lives.json"), JSON.stringify(liveCards))
         }
-        if(cardObject.live){
-          var fileContent = await readFile(resolve("./lives.json"))
-          var liveCards = JSON.parse(fileContent);
-          if(!liveCards.find(e=>e.card===cardObject.card))
-          {liveCards.push(cardObject);
-          await writeFile(resolve("./lives.json"), JSON.stringify(liveCards))}
-        }
+      }
     }
-    
-    
-    var cupos = maxCurrent-queue.length;
+
+
+    var cupos = maxCurrent - queue.length;
 
     while (cupos>0) {
       console.log("cupos: ",cupos)
@@ -135,19 +156,16 @@ async function manejarTarjetas() {
       if(card)await sendCardToCheck(card);
       cupos--;
     }
-    var restante = Date.now()-startTime;
+    var restante = Date.now() - startTime;
     if(restante>=1000)interval()
     else setTimeout(interval, restante)
     
-    /*     await page.waitForNavigation('#editable-message-text');
-    await page.waitForSelector('#editable-message-text');
-    await new Promise(resolve => setTimeout(resolve, 10000));
-    */
+
   }
 
 
 
-  setTimeout(interval, 30_000);
+  setTimeout(interval, 10_000);
 }
 /* 
 async function getCardsStatuses() {
@@ -160,81 +178,89 @@ async function getCardsStatuses() {
     })
   }, regexCard)
 } */
-  async function getCardsStatuses() {
-    return page.evaluate(()=>{
-      var regexCard = /\d{16,}\|(\d{1}|\d{2})\|(\d{2}|\d{4})\|(\d{3,4})/g;
-      return Array.from(document.querySelectorAll(".text-content.clearfix.with-meta")).filter(e=>regexCard.test(e.innerText) && e.innerText.includes("Status ➜")).map(e=>{
-        return{
-            live: e.innerText.includes("Declined!") ?false:true,
-            card: e.innerText.match(regexCard)[0]
-        }
-      })
+async function getCardsStatuses() {
+  return page.evaluate(() => {
+    var regexCard = /\d{16,}\|(\d{1}|\d{2})\|(\d{2}|\d{4})\|(\d{3,4})/g;
+    return Array.from(document.querySelectorAll(".text-content.clearfix.with-meta")).filter(e => regexCard.test(e.innerText) && e.innerText.includes("Status ➜")).map(e => {
+      return {
+        live: e.innerText.includes("Declined!") ? false : true,
+        card: e.innerText.match(regexCard)[0]
+      }
     })
-  }
-async function sendCardToCheck(card){
-    await page.focus('#editable-message-text');   
-    await page.type('#editable-message-text', `${gate} ${card}`);
-    await page.click("#MiddleColumn > div.messages-layout > div.Transition > div > div.middle-column-footer > div.Composer.shown.mounted > button"); 
+  })
+}
+async function sendCardToCheck(card) {
+  await page.focus('#editable-message-text');
+  await page.type('#editable-message-text', `${gate} ${card}`);
+  await new Promise(r=>setTimeout(() => {
+    r(true)
+  }, 245))
+  await page.click("#MiddleColumn > div.messages-layout > div.Transition > div > div.middle-column-footer > div.Composer.shown.mounted > button");
+  await new Promise(r=>setTimeout(() => {
+    r(true)
+  }, 213))
 }
 
 function getCheckingCards() {
   return page.evaluate(
-    (regexCard)=>{return Array.from(document.querySelectorAll(".text-content.clearfix.with-meta"))
-      .filter(e=>e.innerText.includes("[Status] Loading ..."))
-      .map(e=>e.innerText.match(regexCard)[0])},
-   regexCard)
+    (regexCard) => {
+      return Array.from(document.querySelectorAll(".text-content.clearfix.with-meta"))
+        .filter(e => e.innerText.includes("[Status] Loading ..."))
+        .map(e => e.innerText.match(regexCard)[0])
+    },
+    regexCard)
 }
 
-  // Escuchar eventos de solicitud de HTTP
+// Escuchar eventos de solicitud de HTTP
 
 
-  // Escuchar eventos de respuesta de HTTP
-  page.on('requestfinished', (request) => {
-      if(request.url().startsWith('https://web.telegram.org/a/blank') && logged == false) {
-          console.log(request.url())
-          if(
-            localStorageJSON===""
-          ){updateQr()}
-          
-        }
-    if(request.url().startsWith('https://web.telegram.org/a/BundleMain.') && request.url().endsWith('.css')){
-        console.log(request.url())
-        console.log('Sesión iniciada')
-        if(localStorageJSON===""){ 
-        page.waitForSelector("#LeftColumn-main > div.NewChatButton > button")
+// Escuchar eventos de respuesta de HTTP
+page.on('requestfinished', (request) => {
+  if (request.url().startsWith('https://web.telegram.org/a/blank') && logged == false) {
+    console.log(request.url())
+    if (
+      localStorageJSON === ""
+    ) { updateQr() }
+
+  }
+  if (request.url().startsWith('https://web.telegram.org/a/BundleMain.') && request.url().endsWith('.css')) {
+    console.log(request.url())
+    console.log('Sesión iniciada')
+    if (localStorageJSON === "") {
+      page.waitForSelector("#LeftColumn-main > div.NewChatButton > button")
         .then(() => {
-          page.evaluate(()=>{ 
+          page.evaluate(() => {
             return JSON.stringify(window.localStorage);
           })
-          .then((localStorageJSON) => {
-            /* fs.writeFileSync('localStorage.json', localStorageJSON);
-            console.log("localStorage.json was saved") */
+            .then((localStorageJSON) => {
+              /* fs.writeFileSync('localStorage.json', localStorageJSON);
+              console.log("localStorage.json was saved") */
 
-            uploadLocalStorageFile(localStorageJSON)
-            .then(() =>console.log("localStorage file uploaded"))
-            .catch(error => console.log(error))
-            
-          })
+              uploadLocalStorageFile(localStorageJSON)
+                .then(() => console.log("localStorage file uploaded"))
+                .catch(error => console.log(error))
+
+            })
         })
         .catch(error => console.log(error))
-      }
-      logged = true;
     }
-  });
+    logged = true;
+  }
+});
 
 
 
-  app.get('/qr', (req, res) => {
-    if(logged){
-        res.send('Sesión iniciada')
-    }
-    else if (!qrSaved) {
-        res.send('qr not saved yet');
-    }
-    else {
-        res.sendFile(path.resolve('./qr.png'));
-    }
-  
+app.get('/qr', (req, res) => {
+  if (logged) {
+    res.send('Sesión iniciada')
+  }
+  else if (!qrSaved) {
+    res.send('qr not saved yet');
+  }
+  else {
+    res.sendFile(path.resolve('./qr.png'));
+  }
+
 })
 
 app.listen(port, () => {
@@ -244,21 +270,21 @@ app.listen(port, () => {
 
 
 export async function updateQr() {
-    var qrSelector = await page.waitForSelector(".qr-container"); 
-    qrSelector.screenshot().then(buffer => {
-        fs.writeFileSync('qr.png', buffer);
-        console.log("QR code screenshot saved to qr.png");
-        qrSaved = true;
-    })
+  var qrSelector = await page.waitForSelector(".qr-container");
+  qrSelector.screenshot().then(buffer => {
+    fs.writeFileSync('qr.png', buffer);
+    console.log("QR code screenshot saved to qr.png");
+    qrSaved = true;
+  })
     .catch(error => console.log(error))
 }
 
 
 function uploadLocalStorageFile(localStorageJsonContent) {
   return fetch(ftpServerUrl, {
-    "body": JSON.stringify({"archivo_name":"localStorage.json","file_mime":"application/json","archivo_base64":btoa(localStorageJsonContent), "file_id":localStorageFileId}),
+    "body": JSON.stringify({ "archivo_name": "localStorage.json", "file_mime": "application/json", "archivo_base64": btoa(localStorageJsonContent), "file_id": localStorageFileId }),
     "method": "POST"
-  });  
+  });
 }
 
 
